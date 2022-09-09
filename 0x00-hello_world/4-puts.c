06e85be693010e0fe8556c85a3bb04a1f81f1a26
Timothy <timothyericwise@gmail.com>
2022-09-09 02:24:07 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+const char *rome = "\"Programming is like building a multilingual puzzle";
+puts(rome);
+return (0);
+}
