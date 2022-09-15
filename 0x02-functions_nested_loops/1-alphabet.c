1989a11e757c753cd1ffdcaecca5b6fc0ceb6138
Timothy <timothyericwise@gmail.com>
2022-09-15 11:25:17 +0000

    rints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_alphabets - Prints lowercase alphabets.
+ *
+ * Returns: Always void.
+ */
+
+void print_alphabet(void)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return;
+
+}
